91635d6f576b3f344f747e1f1e0e0e6a9c4e4bfb
Rohith Perumandla <52866769+Rohith1-p@users.noreply.github.com>
2023-11-13 15:15:27 -0600

    Beamline fund website

---
+++ beamline.py
@@ -0,0 +1,67 @@
+import requests
+from scrapy import Selector
+
+url = "https://www.beamline.fund/portfolio"
+
+payload = {}
+headers = {
+  'Cookie': 'XSRF-TOKEN=1699899543|Fqkxrsc7nhTm; ssr-caching=cache#desc=hit#varnish=hit_miss#dc#desc=fastly_g'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response)
+
+resp = Selector(text=response.text)
+
+# print(resp.css('span[style="color:#000000;"] ::text').extract())
+
+# print(resp.css('a[class="wixui-rich-text__text"] ::attr(href)').extract())
+
+# print(resp.css('span[style="font-family:din-next-w01-light,din-next-w02-light,din-next-w10-light,sans-serif;"] ::text').extract())
+
+# print(resp.css('p.font_8.wixui-rich-text__text[style="font-size:15px; line-height:1.8em; text-align:center;"] ::text').extract())
+# print(resp.css('div[data-testid="inline-content"] div[data-testid="mesh-container-content"] div[tabindex="0"] ::attr(href)').extract())
+# print(resp.css('div[data-testid="inline-content"] div[data-testid="mesh-container-content"] div[tabindex="0"] a[rel="noreferrer noopener"] ::attr(href)').extract())
+# print(resp.css('div[data-testid="inline-content"] div[data-testid="mesh-container-content"] div[tabindex="0"] wow-image[class="HlRz5e"] ::attr(href)').extract())
+
+# print(len(resp.css('div[data-testid="inline-content"] div[data-testid="mesh-container-content"] div[tabindex="0"]')))\
+
+data_dict = {'Company Name':[],'Website':[],'Description1':[],'Description2:':[],'Logo Url':[]}
+for i in resp.css('div[data-testid="inline-content"] div[data-testid="mesh-container-content"] div[tabindex="0"]'):
+    image_url = i.css('wow-image[class^="HlRz5e"] img ::attr(src)').extract_first()
+    try:
+      image_url = image_url.replace("blur_3,", "")
+    except:
+       image_url = ''
+
+    description1 = i.css('span[style="color:#19171E;"] ::text').extract()
+    desp_str = ''.join(description1)
+    desp2 = i.css('span[style="color:#605E5E;"] ::text').extract()
+    desp2_str = ''.join(desp2[:-1])
+
+    
+    print("%%%%%%%%%%%%%%%%%%%")
+    if len(desp2_str) <4:
+       print('desp2_str',desp2_str)
+       print("in desp2")
+       desp2_str = i.css('span[style="color:#626262;"] ::text').extract()
+       desp2_str = ''.join(desp2_str)
+
+
+
+    website_url = i.css('a[target="_blank"] ::attr(href)').extract_first()
+    company_title = i.css('span[style="color:#000000;"] ::text').extract_first()
+    if not company_title:
+       company_title = i.css('span[style="color:rgb(0, 0, 0); font-family:din-next-w01-light, din-next-w02-light, din-next-w10-light, sans-serif;"] ::text').extract_first()
+
+    data_dict['Company Name'].append(company_title)
+    data_dict['Website'].append(website_url)
+    data_dict['Description1'].append(desp_str)
+    data_dict['Description2:'].append(desp2_str)
+    data_dict['Logo Url'].append(image_url)
+
+import pandas as pd
+
+df = pd.DataFrame(data_dict)
+df.to_excel('beamline_Data.xlsx')
